7786cb7ea7736bb239d198515abe9f0ebf6a32eb
Jayesh <jayeshchau598007@gmail.com>
2021-08-29 11:52:30 +0530

    files added

--- Automate_whatsapp_msg/send_what_msg.py
+++ Automate_whatsapp_msg/send_what_msg.py
@@ -1,5 +1,5 @@
 import pywhatkit as kit
-kit.sendwhatmsg( phone_no="+918275590195", message="Hello, this is Jayesh", time_hour= 21, time_min=15,wait_time= 10)
+kit.sendwhatmsg( phone_no="+918888888888", message="Hello, this is Jayesh", time_hour= 21, time_min=15,wait_time= 10)
 
 # this program send the whatsapp message to the mob number at given time
 
